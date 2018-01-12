d9a68900a6aca41acdf918d41463bfd0340b2b04
Worseize <worseize@gmail.com>
2018-01-12 23:05:49 +0200

    small bug fixing
    bug fixed (twice update bug)

--- eternagame.js
+++ eternagame.js
@@ -1,4 +1,4 @@
-let consoleLog = 0 , memory = [], showMenu = true, autoMode = false, memoryPosition = 0 , scaler = 20, a = 0, kof = 0.6 , buttonWidth = 210 , buttonHeight = 70, oligo1 = [], oligo2 = [] , rna1 = [] , rna2 = [], myOligo1, myOligo2, myRna, myRna2, start = false, errors = [] , correction = 30;
+let flag = 0 , consoleLog = 0 , memory = [], showMenu = true, autoMode = false, memoryPosition = 0 , scaler = 20, a = 0, kof = 0.6 , buttonWidth = 210 , buttonHeight = 70, oligo1 = [], oligo2 = [] , rna1 = [] , rna2 = [], myOligo1, myOligo2, myRna, myRna2, start = false, errors = [] , correction = 30;
 
 function setup(){
   ol1 = createElement("input","text");
@@ -45,24 +45,26 @@ function setup(){
 
 function draw(){
   if(start){
-    background(12);
-    errorsFinder(); //Find and shows basic errors( wrong inputs )
-    myButton.show();
-    calculate();
-    if(autoMode === false){
-      myOligo1.show();
-      myOligo2.show();
-      myRna.show(); // RNA variant 1
-      myRna2.show(); // if more than 1 variant , that RNA shows up
-    }else{
-       //autoMode
+    for(let ooo = 0; ooo < 2; ooo++){ // Strange bug , fixed by applying loop 
+      background(12);
+      calculate();
+      errorsFinder(); //Find and shows basic errors( wrong inputs )
+      myButton.show();
+      if(autoMode === false){
+        myOligo1.show();
+        myOligo2.show();
+        myRna.show(); // RNA variant 1
+        myRna2.show(); // if more than 1 variant , that RNA shows up
+      }else{
+         //autoMode
+      }
+      showMemory();
+      if(showMenu){
+        showFunctionality(); // shows all options that exist
+      }
+      start = false;
+      consoleLog++;
+      console.log("calculated " + consoleLog + " times" )
     }
-    showMemory();
-    if(showMenu){
-      showFunctionality(); // shows all options that exist
-    }
-    start = false;
-    consoleLog++;
-    console.log("calculated " + consoleLog + " times" )
   }
 }
